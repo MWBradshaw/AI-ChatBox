d4fb4d476fc1aa06ef471e0b915e4cd807d362d2
MWBradshaw <mbradshaw@claflin.edu>
2023-01-08 14:22:13 -0500

    This commit will update the AI Chatbot repo

--- training.py
+++ training.py
@@ -6,8 +6,8 @@ import numpy as np
 import nltk
 from nltk.stem import WordNetLemmatizer
 
-from tensorflow.keras.models import sequential
-from tensorflow.keras.layers import Dence, Activation, Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer
@@ -22,3 +22,6 @@ ignore_letters = ['?', '!', '.', ',']
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         word_list = nltk.tokenize(pattern)
+        words.append(word_list)
+        documents.append((word_list), intent['tag'])
+        
